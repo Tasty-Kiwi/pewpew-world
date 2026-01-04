ecbc6475f8a150980bbb6f7b882f1a502fab9811
Artemii Kravchuk <mail@artiekra.org>
2026-01-04 21:15:29 +0200

    [front] update favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -13,6 +13,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "PewPew World",
   description: "PewPew World",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
